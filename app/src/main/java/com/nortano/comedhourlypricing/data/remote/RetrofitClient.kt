7d2f2ab79d8fb7b5c5fd6a03fdf211310e30cdc6
gitjiggywithit <22042746+gitjiggywithit@users.noreply.github.com>
2026-04-19 22:54:04 -0500

    chore: resolve agp 10 deprecation warnings (#2)

--- app/src/main/java/com/nortano/comedhourlypricing/data/remote/RetrofitClient.kt
+++ app/src/main/java/com/nortano/comedhourlypricing/data/remote/RetrofitClient.kt
@@ -1,6 +1,6 @@
 package com.nortano.comedhourlypricing.data.remote
 
-import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
+import retrofit2.converter.kotlinx.serialization.asConverterFactory
 import kotlinx.serialization.json.Json
 import okhttp3.MediaType.Companion.toMediaType
 import okhttp3.OkHttpClient
