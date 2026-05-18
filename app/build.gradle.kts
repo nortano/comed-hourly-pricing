f9776c929e2a8f7c34cb0eab63eb3d38e6bc2270
gitjiggywithit <22042746+gitjiggywithit@users.noreply.github.com>
2026-05-17 22:08:13 -0500

    chore: bump to 1.0.6 (#12)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,8 +18,8 @@ android {
         applicationId = "com.nortano.comedhourlypricing"
         minSdk = 30
         targetSdk = 34
-        versionCode = 6
-        versionName = "1.0.5"
+        versionCode = 7
+        versionName = "1.0.6"
         vectorDrawables {
             useSupportLibrary = true
         }
